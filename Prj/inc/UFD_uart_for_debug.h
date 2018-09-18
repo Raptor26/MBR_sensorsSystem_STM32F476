4a939c3a3b80cc7f4a2b621bea94e82b01cce7bc
Isaev Mickle <mrraptor26@gmail.com>
2018-09-18 16:57:42 +0300

    feature ()
    - add empty function in main.c
    - add files UFD
    - add tasks for students in UFD files

--- Prj/inc/UFD_uart_for_debug.h
+++ Prj/inc/UFD_uart_for_debug.h
@@ -1,5 +1,5 @@
 /**
- * @file   	main.h
+ * @file   	UFD_uart_for_debug.h
  * @author
  * @version
  * @date 	18 сент. 2018 г.
@@ -7,23 +7,23 @@
  */
 
 
-#ifndef INC_MAIN_H_
-#define INC_MAIN_H_
+#ifndef UFD_UART_FOR_DEBUG_H_
+#define UFD_UART_FOR_DEBUG_H_
 
 
 /*#### |Begin| --> Секция - "Include" ########################################*/
 /*==== |Begin| --> Секция - "C libraries" ====================================*/
 #include "stdio.h"
 #include "stdint.h"
-#include "math.h"
 /*==== |End  | <-- Секция - "C libraries" ====================================*/
 
 /*==== |Begin| --> Секция - "MK peripheral libraries" ========================*/
 #include "stm32l4xx.h"
+#include "stm32l4xx_hal.h"
+#include "stm32l4xx_ll_usart.h"
 /*==== |End  | <-- Секция - "MK peripheral libraries" ========================*/
 
 /*==== |Begin| --> Секция - "Extern libraries" ===============================*/
-#include "rcc.h"
 /*==== |End  | <-- Секция - "Extern libraries" ===============================*/
 /*#### |End  | <-- Секция - "Include" ########################################*/
 
@@ -41,13 +41,26 @@
 
 
 /*#### |Begin| --> Секция - "Прототипы глобальных функций" ###################*/
+extern void
+UFD_Init_All_USART2_TxRx_DMA1_Channel7_IO_Ports(
+	uint32_t baudrate);
+
+extern void
+UFD_StartForceDMATransmit(
+	uint32_t *pMemSource,
+	uint16_t cnt);
+
+extern void
+UFD_StartDMATransmit(
+	uint32_t *pMemSource,
+	uint16_t cnt);
 /*#### |End  | <-- Секция - "Прототипы глобальных функций" ###################*/
 
 
 /*#### |Begin| --> Секция - "Определение макросов" ###########################*/
 /*#### |End  | <-- Секция - "Определение макросов" ###########################*/
 
-#endif	/* INC_MAIN_H_ */
+#endif	/* UFD_UART_FOR_DEBUG_H_ */
 
 /*############################################################################*/
 /*################################ END OF FILE ###############################*/
